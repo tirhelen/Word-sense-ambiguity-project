3968393429707019b551731dccd85be73094f493
Helen Tirkkonen <helen.tirkkonen@helsinki.fi>
2025-12-01 13:15:15 +0200

    Pick seeds function

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import re
 import math
-from collections import Counter, defaultdict
 
 def read_file(path):
     with open(path, "r", encoding="utf-8") as f:
@@ -18,7 +17,35 @@ def load_corpus(corpus_dir):
         documents.append(tokenize(text))
     return documents
 
+def pick_seeds(corpus, word1, word2, k=5):
+    seeds = []
+    combined_word = word1 + word2
+
+    # Sense 0 -> came from word1, Sense 1 -> came from word 2
+    sense_map = {
+        f"ORIGINAL={word1}": 0,
+        f"ORIGINAL={word2}": 1}
+
+    for document in corpus:
+        tokens = document
+        for i, token in enumerate(tokens):
+
+            if token == combined_word:
+                # Context window of size k to the left/right of the target
+                left = tokens[max(0, i-k): i]
+                right = tokens[i+1: i+k+1]
+                # Creating an instance of the window
+                instance = (left, token, right)
+                # Checking if word1 or word2 appears in the context
+                for marker, sense_id in sense_map.items():
+                    if marker in left or marker in right:
+                        seeds.append((instance, sense_id))
+                        break
+    return seeds
+
+
 if __name__ == "__main__":
     corpus_dir = "./Corpus-spell-AP88"
     corpus = load_corpus(corpus_dir)
+    print(pick_seeds(corpus,"car","speech"))
     
